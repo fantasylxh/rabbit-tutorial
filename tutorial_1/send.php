35f99ed4d5a521749d384efe2b6daf61e2d91116
lostofland <772932587@qq.com>
2018-10-11 16:31:33 +0800

    update

--- tutorial_1/send.php
+++ tutorial_1/send.php
@@ -1,6 +1,6 @@
 <?php
-$config = require "./config.php";
-require_once __DIR__ . '/vendor/autoload.php';
+$config = require "../config.php";
+require_once '../vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
@@ -9,19 +9,13 @@ $connection = new AMQPStreamConnection($config['rabbitmq']['host'], $config['rab
     $config['rabbitmq']['login'], $config['rabbitmq']['password'], $config['rabbitmq']['vhost']);
 $channel = $connection->channel();
 
+//发送方其实不需要设置队列， 不过对于持久化有关，建议执行该行
 $channel->queue_declare('queue_one', false, false, false, false);
 
-echo ' [*] 等待消息. 退出按 CTRL+C', "\n";
+$msg = new AMQPMessage('游戏爱好者!荒野大镖客2发布');
+$channel->basic_publish($msg, '', 'queue_one');
 
-$callback = function($msg) {
-    echo " [x] 收到消息 ", $msg->body, "\n";
-};
-
-$channel->basic_consume('queue_one', '', false, true, false, false, $callback);
-
-while(count($channel->callbacks)) {
-    $channel->wait();
-}
+echo " [x] Sent Message ok\n";
 
 $channel->close();
 $connection->close();
