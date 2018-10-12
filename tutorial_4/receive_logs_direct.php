ffe80a13fdf6c9bc1eb3d261dbfafdfc5625b6a6
lostofland <772932587@qq.com>
2018-10-12 17:10:45 +0800

    add tutorial

---
+++ tutorial_4/receive_logs_direct.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once '../vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$connection = new AMQPStreamConnection('192.168.20.185', 5672, 'rabbit', '123456');
+$channel = $connection->channel();
+
+$channel->exchange_declare('direct_logs', 'direct', false, false, false);
+
+list($queue_name, ,) = $channel->queue_declare("", false, false, true, false);
+
+$severities = array_slice($argv, 1);
+if(empty($severities )) {
+    file_put_contents('php://stderr', "Usage: $argv[0] [info] [warning] [error]\n");
+    exit(1);
+}
+
+foreach($severities as $severity) {
+    $channel->queue_bind($queue_name, 'direct_logs', $severity);
+}
+
+echo ' [*] 等待消息. 退出按 CTRL+C', "\n";
+
+$callback = function($msg){
+    echo ' [x] ',$msg->delivery_info['routing_key'], ':', $msg->body, "\n";
+};
+
+$channel->basic_consume($queue_name, '', false, true, false, false, $callback);
+
+while(count($channel->callbacks)) {
+    $channel->wait();
+}
+
+$channel->close();
+$connection->close();
+
+?>
